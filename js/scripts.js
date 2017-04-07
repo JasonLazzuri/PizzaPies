8ac28e0837396235d9977ecf4a2b9f5e282ed6c8
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-04-07 12:44:51 -0700

    be finished just before I start to style things

--- js/scripts.js
+++ js/scripts.js
@@ -5,50 +5,70 @@ function Pizza(size, topping1, topping2, topping3) {
   this.topping3 = topping3;
 }
 
-function sizePrice(size){
+
+// function topping(topping1, topping2, topping3){
+//   var toppingTotal = 0;
+//   if
+// }
+
+
+
+
+
+function totalPrice(size, topping1, topping2, topping3){
   var cost = 10;
-  if (size === 4){
-    cost += 8;
-  }else if(size === 3){
-    cost += 6;
-  }else if(size === 2){
-    cost += 4;
-  }return cost;
+  if((size === 4)&&(topping1 === 1)&&(topping2 === 2)&&(topping3 === 3)){
+    cost +=12;
+  }else if((size === 4)&&(topping1 === 1)&&(topping2 === 2)){
+    cost +=10;
+  }else if ((size === 4)&&(topping1 === 1)){
+      cost += 9;
+  }else if((size === 3)&&(topping3 === 3)&&(topping2 === 2)&&(topping1 === 1)){
+    cost +=10;
+  }else if((size === 3)&&(topping2 === 2)&&(topping1 === 1)){
+    cost +=8;
+  }else if ((size === 3)&&(topping1 === 1)){
+      cost += 7;
+  }else if((size === 2)&&(topping3 === 3)&&(topping2 === 2)&&(topping1 === 1)){
+      cost +=8;
+    }else if((size === 2)&&(topping2 === 2)&&(topping1 === 1)){
+      cost +=6;
+    }else if ((size === 2)&&(topping1 === 1)){
+        cost += 5;
+    }else if((size === 1)&&(topping3 === 3)&&(topping2 === 2)&&(topping1 === 1)){
+        cost +=7;
+      }else if((size === 1)&&(topping2 === 2)&&(topping1 === 1)){
+        cost +=6;
+      }else if ((size === 1)&&(topping1 === 1)){
+          cost += 5;
+        }return "$" + cost + ".00.";
 }
 
-function toppingPrice(topping1, topping2, topping3){
-  var cost = sizePrice();
-  if (topping1 === 1){
-    cost += 2;
-  }else if(topping2 === 2){
-    cost += 3;
-  }else if(topping3 === 3){
-    cost += 4;
-  }return cost;
+
+Pizza.prototype.result = function () {
+return  "Thanks for Ordering! Your pizza will be ready soon! Your Total is " + cost
 }
 
-Pizza.prototype.totalCost = function () {
-  return sizePrice() + toppingPrice();
-};
 
 $(document).ready(function(){
   $("#form").submit(function(event) {
     event.preventDefault();
 
     var size = parseInt($("#size").val());
-    var top1 = parseInt($("#topping1").val());
-    var top2 = parseInt($("#topping2").val());
-    var top3 = parseInt($("#topping3").val());
+    var topping1 = parseInt($("#topping1").val());
+    var topping2 = parseInt($("#topping2").val());
+    var topping3 = parseInt($("#topping3").val());
 
     var newPizza = new Pizza(size, topping1, topping2, topping3);
-    cost = Pizza(newPizza.size, newPizza.topping1, newPizza.topping2, newPizza.topping3);
+    cost = totalPrice(newPizza.size, newPizza.topping1, newPizza.topping2, newPizza.topping3);
 
     $("#size").text("");
     $("#topping1").text("");
     $("#topping2").text("");
     $("#topping3").text("");
 
-    $("#totalCost").text(cost);
+
+    $("#totalCost").text(newPizza.result);
     $("#order").show();
   });
 });
